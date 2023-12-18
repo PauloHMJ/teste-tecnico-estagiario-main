3f3a84135c2c9d1df904fa63e26b51db466a3381
PauloHMJ <114310864+PauloHMJ@users.noreply.github.com>
2023-12-18 01:23:03 -0300

    Update page.jsx

--- src/app/questoes/3/page.jsx
+++ src/app/questoes/3/page.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const numerosInteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const CalcularSomaPares = (numeros) => {
-  return numeros.reduce ((soma, numero) => {
-    if (numero % 2 === 0){
+  return numeros.reduce((soma, numero) => {
+    if (numero % 2 === 0) {
       return soma + numero;
     }
     return soma;
@@ -12,22 +12,19 @@ const CalcularSomaPares = (numeros) => {
 };
 
 export default function Questao3() {
-
-  const SomaPares = CalcularSomaPares(numerosInteiros);
+  const somaPares = CalcularSomaPares(numerosInteiros);
 
   return (
     <>
       <h1>Questão 3</h1>
 
-      |<p>Soma: {SomaPares}</p>
 
-     {/* <p>
+      <p>Soma dos números pares: {somaPares}</p>
+
+           {/* <p>
         Escreva uma função JavaScript que recebe uma matriz de números inteiros
         e retorna a soma de todos os números pares. Exiba a soma na tela.
-      </p>
-      <p>Soma:</p>
-      */}
-
+      </p> */}
     </>
   );
 }
