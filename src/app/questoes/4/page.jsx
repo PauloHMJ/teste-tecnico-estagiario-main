8e61798fdb4f8630055a63e81fd804e69c6a024b
PauloHMJ <114310864+PauloHMJ@users.noreply.github.com>
2023-12-18 01:24:18 -0300

    Update page.jsx

--- src/app/questoes/4/page.jsx
+++ src/app/questoes/4/page.jsx
@@ -37,11 +37,11 @@ export default function Questao4() {
 
       <ProductList produtos={produtos} />
 
-      <p>
+     {/* <p>
         Crie um componente chamado `ProductList` que recebe uma matriz de
         objetos de produto e renderiza uma lista de produtos. Cada produto deve
         exibir seu nome e preço.
-      </p>
+  </p>*/}
     </>
   );
 }
